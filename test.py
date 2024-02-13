567395201cef551a1e78942c1bddc8571062de8d
Mauceri <cmauceri@gmail.com>
2024-02-13 14:43:27 +0100

    Commentaire idiot

--- test.py
+++ test.py
@@ -6,11 +6,12 @@ from nio.events.room_events import RoomMessageText
 logger = logging.getLogger(__name__)
 
 class Echo(IObserver):
-    def __init__(self,observable:Callbacks=None):
+    def __init__(self,observable:IObservable=None):
         self.observable =observable
 
     async def notify(self,room:MatrixRoom, event:RoomMessageText, msg:str):
         logger.info(f"***************************** L'utilisateur {event.sender} a écrit {msg} depuis ls salon {room.name}")
+        # Coco répète ce qu'on lui dit
         await self.observable.notify(room,event,f"L'utilisateur {event.sender} a écrit {msg} depuis le salon {room.name}")
 
     def prefix(self):
