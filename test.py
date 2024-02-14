532863b6a2e401e85d0929984e086f13ec77333e
Mauceri <cmauceri@gmail.com>
2024-02-14 13:43:03 +0100

    Nouvelle interface IObservable

--- test.py
+++ test.py
@@ -12,20 +12,20 @@ class Echo(IObserver):
     async def notify(self,room:MatrixRoom, event:RoomMessageText, msg:str):
         logger.info(f"***************************** L'utilisateur {event.sender} a écrit {msg} depuis ls salon {room.name}")
         # Coco répète ce qu'on lui dit
-        await self.observable.notify(room,event,f"L'utilisateur {event.sender} a écrit {msg} depuis le salon {room.name}")
+        await self.__observable.notify(room,event,f"L'utilisateur {event.sender} a écrit {msg} depuis le salon {room.name}",None,None)
 
     def prefix(self):
         return "!echo"
     
 class Plugin(IPlugin):
     def __init__(self,observable:IObservable):
-        self.observable = observable
-        self.echo = Echo(observable)
+        self.__observable = observable
+        self.echo = Echo(self.__observable)
         logger.info(f"********************** Observateur créé {self.echo.prefix()}")
         
     def start(self):
         logger.info(f"********************** Inscripton de {self.echo.prefix()}")
-        self.observable.subscribe(self.echo)
+        self.__observable.subscribe(self.echo)
 
     async def stop(self):
         pass
