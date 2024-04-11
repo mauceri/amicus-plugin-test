941ab421b3cb522144261cbc1733c4847866c1dd
Mauceri <cmauceri@gmail.com>
2024-04-11 12:55:31 +0200

    maj amicus_interfaces

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ logger = logging.getLogger(__name__)
 
 class Echo(IObserver):
     def __init__(self,observable:IObservable=None):
-        self.observable =observable
+        self.__observable =observable
 
     async def notify(self,room:MatrixRoom, event:RoomMessageText, msg:str):
         logger.info(f"***************************** L'utilisateur {event.sender} a écrit {msg} depuis ls salon {room.name}")
         # Coco répète ce qu'on lui dit
-        await self.observable.notify(room,event,f"L'utilisateur {event.sender} a écrit {msg} depuis le salon {room.name}",None,None)
+        await self.__observable.notify(room,event,f"L'utilisateur {event.sender} a écrit {msg} depuis le salon {room.name}",None,None)
 
     def prefix(self):
         return "!echo"
@@ -21,6 +21,7 @@ class Plugin(IPlugin):
     def __init__(self,observable:IObservable,path:str):
         super().__init__(observable,str)
         self.echo = Echo(observable)
+        self.__observable = observable
         logger.info(f"********************** Observateur créé {self.echo.prefix()}")
         
     def start(self):
